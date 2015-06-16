caabdb3fb5ed1cb676ac08ac84b9b8b8acc094da
Edwin Hollen <edwinhollen@gmail.com>
2015-06-16 09:54:50 -0400

    Initial commit with build file

---
+++ build.py
@@ -0,0 +1,43 @@
+import json
+import sys
+import os
+import glob
+import tempfile
+import shutil
+from subprocess import Popen
+
+def build(config):
+    # make build dir if it doesn't exist
+    os.makedirs(config["output"], exist_ok=True)
+    # assemble list of source files
+    source_files = []
+    for src_str in config["sources"]:
+        source_files += glob.glob(src_str)
+    # make temporary dir to hold all source files
+    with tempfile.TemporaryDirectory() as tempDir:
+        # copy source files
+        for source_file in source_files:
+            shutil.copy(source_file, tempDir)
+        # make zip of temp dir
+        zip = shutil.make_archive(
+            os.path.join(config["output"], "{} v{}".format(config["title"], config["version"])),
+            "zip",
+            tempDir
+        )
+        # rename to .love
+        love = shutil.move(zip, os.path.splitext(zip)[0]+".love")
+        print("Created {}".format(love))
+
+def run(config):
+    love_path = config["love_path"] or shutil.which("love")
+    love_file = glob.glob(os.path.join(config["output"], "*.love"))[0]
+    Popen([love_path, love_file])
+
+def config(filename="project.json"):
+    with open(filename, encoding="utf-8") as raw_json:
+        return json.loads(raw_json.read())
+
+if __name__ == "__main__":
+    build(config())
+    if "--run" in sys.argv:
+        run(config())
